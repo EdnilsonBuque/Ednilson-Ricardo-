8b4c87becb407585a2e34a7f791f448b0f00722d
Ednilson Ricardo Lucas Buque <150295579+EdnilsonBuque@users.noreply.github.com>
2024-08-02 13:54:05 +0200

    adiçao de novos arquivos

--- meuprojectos/views.py
+++ meuprojectos/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     """Pagina principal de meuprojeto"""
     return render(request, 'meuprojectos/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
